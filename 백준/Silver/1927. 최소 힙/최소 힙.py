d17bf4c802de5269389059c3197288d65f0cc087
Lee SangBong <77820303+bong7233@users.noreply.github.com>
2022-03-25 11:46:01 +0900

    [Silver II] Title: 최소 힙, Time: 180 ms, Memory: 36788 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+
+h = []
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    c = int(sys.stdin.readline())
+    if c == 0:
+        try:
+            print(heapq.heappop(h))
+        except:
+            print(0)
+    else:
+        heapq.heappush(h,c)
+
+            
