4f73378e96cd0cff4ba3548e35343af691e3b3eb
Lee SangBong <77820303+bong7233@users.noreply.github.com>
2022-03-27 15:36:43 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,13 +1,12 @@
-import re
+arr = input().split('-')  # 입력값을 -기준으로 나눈다
+# - 기준으로 나눈 값들중 맨앞값을 제외한 나머지를 모두빼주면 최소값이 된다
 
-cal = input()
+cnt = 0
+for i in arr[0].split('+'): # 값들을 각각 +값으로 다시 나눠서 숫자만 남김
+    cnt += int(i)           # 첫번째 값은 더해준다
+for j in arr[1:]:           # 이후값들은 전부 빼준다
+    for n in j.split('+'):
+        cnt -= int(n)
+print(cnt)
 
-cal_sep = cal.split('-')
-cc = [re.sub(r'\b0+(?!\b)','',i) for i in cal_sep]
 
-total = eval(cc[0])
-
-for i in range(1,len(cc)):
-    total -= eval(cc[i])
-
-print(total)
