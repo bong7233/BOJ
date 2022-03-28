d24bde6ee046c734954e29d15fe109410dff5ce7
Lee SangBong <77820303+bong7233@users.noreply.github.com>
2022-03-28 13:13:20 +0900

    [Silver IV] Title: 국영수, Time: 472 ms, Memory: 79284 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -6,10 +6,15 @@ for _ in range(n):
     student = list(stdin.readline().split())
     students.append(student)
 
-sort_n = sorted(students, key=lambda student: student[0])
-sort_m = sorted(sort_n, key=lambda student: int(student[3]), reverse=True)
-sort_e = sorted(sort_m, key=lambda student: int(student[2]))
-sort_k = sorted(sort_e, key=lambda student: int(student[1]), reverse=True)
+# 이런식의 코드도 가능하지만 왜 역순으로 작동하는지는 
+# students.sort(key=lambda student: student[0])                       # 이름 오름차순
+# students.sort(key=lambda student: int(student[3]), reverse=True)    # 수학 내림차순
+# students.sort(key=lambda student: int(student[2]))                  # 영어 오름차순
+# students.sort(key=lambda student: int(student[1]), reverse=True)    # 국어 내림차순
 
-for student in sort_k:
+# 튜플을 활용하여 원하는 정렬기준을 순서대로 입력 (국어,영어,수학,이름) 
+# 오름차순은 음수처리로 간단하게 해결
+students.sort(key=lambda student: (-int(student[1]), int(student[2]), -int(student[3]), student[0]))
+
+for student in students:
     print(student[0])
